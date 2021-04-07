d5d6bde08d2453350301aa1183863779b63da776
Martin Moradi <moradi.martin@gmail.com>
2021-04-07 18:53:57 +0200

    section image tiles + heading

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 import Cursor from './cursor';
 import Grid from './grid';
 import { preloadImages, animateButton } from './utils';
+import LocomotiveScroll from 'locomotive-scroll';
+
+const button = document.querySelector('.button');
 
 // Preload  images
 preloadImages('.grid__item-img, .bigimg').then(() => {
@@ -9,8 +12,25 @@ preloadImages('.grid__item-img, .bigimg').then(() => {
 
   // Initialize grid
   const grid = new Grid(document.querySelector('.grid'));
+
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+  });
+
+  document.querySelector('.button').onmousemove = (e) => {
+    button.classList.add('button-hover');
+    button.classList.remove('border-gradient');
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+    e.target.style.setProperty('--x', `${x}px`);
+    e.target.style.setProperty('--y', `${y}px`);
+  };
+
+  document.querySelector('.button').onmouseleave = (e) => {
+    button.classList.remove('button-hover');
+    button.classList.add('border-gradient');
+  };
 });
 
-const cursor = new Cursor(document.querySelector('.cursor'));
-
-animateButton();
+// const cursor = new Cursor(document.querySelector('.cursor'));
